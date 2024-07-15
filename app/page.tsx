86cfbf1f41b319fb99529499d2c75bfd8586f780
eunicerodas <114092041+eunicerodas@users.noreply.github.com>
2024-07-15 17:14:33 -0600

    Update page.tsx
    fix register unknow register function

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,21 @@ import { Search } from './search';
 import { Form } from './form';
 import { SubmitButton } from './submit-button';
 
+async function register(formData: FormData) {
+  'use server';
+  /*let email = formData.get('email') as string;
+  let password = formData.get('password') as string;
+  let user = await getUser(email);
+
+  if (user.length > 0) {
+    return 'User already exists'; // TODO: Handle errors with useFormStatus
+  } else {
+    await createUser(email, password);
+    redirect('/login');*/
+  }
+}
+
+
 export default async function IndexPage({
   searchParams
 }: {
