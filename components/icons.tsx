285b714ca663f5d6da187dfee8f191b2f80b5ae0
eunicerodas <114092041+eunicerodas@users.noreply.github.com>
2024-07-16 12:44:58 -0600

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -89,9 +89,7 @@ export function Spinner() {
 
 export function Logo() {
   return (
-    <?xml version="1.0" encoding="iso-8859-1"?>
-    <!-- Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
-    <!-- License: CC0. Made by SVG Repo: https://www.svgrepo.com/svg/55261/doctor -->
+
     <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
        viewBox="0 0 496.2 496.2" style="enable-background:new 0 0 496.2 496.2;" xml:space="preserve">
     <path style="fill:#48A1AF;" d="M496.2,248.1C496.2,111.1,385.1,0,248.1,0S0,111.1,0,248.1s111.1,248.1,248.1,248.1
