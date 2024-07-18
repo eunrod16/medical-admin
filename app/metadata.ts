f56129371b15eedad55d87469f87765c3620098b
eunicerodas <114092041+eunicerodas@users.noreply.github.com>
2024-07-18 06:32:54 -0600

    Remove files

--- app/metadata.ts
+++
@@ -1,5 +0,0 @@
-// app/metadata.ts
-export const metadata = {
-  title: 'Medical Admin',
-  description: 'A user admin medical configured with Next.js, Postgres, NextAuth, Tailwind CSS, TypeScript, and Prettier.'
-};
