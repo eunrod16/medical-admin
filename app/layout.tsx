e0c8f95e1230ef2317f797b18e10ca8eb85d279f
eunicerodas <114092041+eunicerodas@users.noreply.github.com>
2024-07-17 21:31:35 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { Analytics } from '@vercel/analytics/react';
 import { Logo, WaitingIcon, UsersIcon, DoctorIcon, RegisterIcon } from '@/components/icons';
 import { User } from './user';
 import { NavItem } from './nav-item';
+import {useState} from 'react';
+import { Button } from '@/components/ui/button';
 
 export const metadata = {
   title: 'Medical Admin',
@@ -17,11 +19,13 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [showHamburguer, setShowHamburguer] = useState(false);
+
   return (
     <html lang="en" className="h-full bg-gray-50">
       <body>
         <div className="grid min-h-screen w-full lg:grid-cols-[280px_1fr]">
-          <div className="hidden border-r bg-gray-100/40 lg:block dark:bg-gray-800/40">
+          <div className={(showHamburguer? "block" : "hidden") + " border-r bg-gray-100/40 lg:block dark:bg-gray-800/40"}>
             <div className="flex h-full max-h-screen flex-col gap-2">
               <div className="flex h-[60px] items-center border-b px-5">
                 <Link
@@ -57,6 +61,9 @@ export default function RootLayout({
           </div>
           <div className="flex flex-col">
             <header className="flex h-14 lg:h-[60px] items-center gap-4 border-b bg-gray-100/40 px-6 dark:bg-gray-800/40 justify-between lg:justify-end">
+              <Button onClick={() => setShowHamburguer(!showHamburguer)}>
+                =
+              </Button>
               <Link
                 className="flex items-center gap-2 font-semibold lg:hidden"
                 href="/"
@@ -75,3 +82,4 @@ export default function RootLayout({
     </html>
   );
 }
+
