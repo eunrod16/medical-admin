8ac29465c930410db12f4f6d6f24da832958c7c0
eunicerodas <114092041+eunicerodas@users.noreply.github.com>
2024-07-17 20:53:22 -0600

    Update nav-item.tsx

--- app/nav-item.tsx
+++ app/nav-item.tsx
@@ -1,34 +1,17 @@
 'use client';
 
-import clsx from 'clsx';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 
-
-
-export function NavItem({
-  href,
-  children,
-  onClick
-}: {
+interface NavItemProps {
   href: string;
   onClick?: () => void;
   children: React.ReactNode;
-}) {
-  const pathname = usePathname();
+}
 
+export function NavItem({ href, onClick, children }: NavItemProps) {
   return (
-    <Link
-      href={href}
-      onClick={onClick}
-      className={clsx(
-        'flex items-center gap-3 rounded-lg  px-3 py-2 text-gray-900  transition-all hover:text-gray-900  dark:text-gray-50 dark:hover:text-gray-50',
-        {
-          'bg-gray-100 dark:bg-gray-800': pathname === href
-        }
-      )}
-    >
+    <a href={href} onClick={onClick} className="flex items-center gap-2 py-2 px-4 rounded-md hover:bg-gray-200 dark:hover:bg-gray-700">
       {children}
-    </Link>
+    </a>
   );
 }
