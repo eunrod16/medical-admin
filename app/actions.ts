ead39616c6f4379b63e7e173a8a108874098248f
eunicerodas <114092041+eunicerodas@users.noreply.github.com>
2024-07-17 18:16:40 -0600

    Update actions.ts

--- app/actions.ts
+++ app/actions.ts
@@ -17,9 +17,15 @@ export async function fetchUsers() {
 }
 
 export type Paciente = {
+  id: number;
   nombre: string | null;
   estado: string | null;
   medico: string | null;
+  direccion: string | null;
+  telefono: string | null;
+  email: string | null;
+  edad: string | null;
+  serial: string | null;
 };
 
 export var pacientesTail: Paciente[] = [];
