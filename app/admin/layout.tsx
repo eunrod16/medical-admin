d8a207ce4f68fe7402dd6f34a06bdca25e0607fa
eunicerodas <114092041+eunicerodas@users.noreply.github.com>
2024-07-18 10:06:01 -0600

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -64,11 +64,11 @@ function DesktopNav() {
     <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
       <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
         <Link
-          href="https://vercel.com/templates/next.js/admin-dashboard-tailwind-postgres-react-nextjs"
-          className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+          href="/admin"
+          className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
         >
-          <Logo className="h-3 w-3 transition-all group-hover:scale-110" />
-          <span className="sr-only">Acme Inc</span>
+          <Logo className="transition-all group-hover:scale-110" />
+          <span className="sr-only">Medical Admin</span>
         </Link>
 
         <NavItem href="/admin" label="Ingreso Pacientes">
@@ -117,11 +117,11 @@ function MobileNav() {
       <SheetContent side="left" className="sm:max-w-xs">
         <nav className="grid gap-6 text-lg font-medium">
           <Link
-            href="#"
+            href="/admin"
             className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
           >
             <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
-            <span className="sr-only">Vercel</span>
+            <span className="sr-only">Medical Admin</span>
           </Link>
           <Link
             href="/admin"
