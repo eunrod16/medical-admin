ac78afc1dd938edd421392558fa5f451e4b5ab87
eunicerodas <114092041+eunicerodas@users.noreply.github.com>
2024-07-26 14:47:08 -0600

    fix inventory google

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,49 +1,40 @@
-// pages/index.tsx
-import { GetServerSideProps } from 'next';
-import { getSheetData } from '@/lib/googleapi'
+// app/inventory/page.tsx
+import { getSheetData } from '@/lib/googleapi';
 import React from 'react';
 
-type Props = {
-  data: string[][];
-};
+const spreadsheetId = process.env.SPREADSHEET_ID || '15P5ZQ2BGTqbl8qmkz2Vt1VOaKPFyx1Df2W_KPf0kT_s'; // Utiliza una variable de entorno para mayor flexibilidad
+const range = 'adults!A:D'; // Cambia el rango según tu hoja de cálculo
 
-const Home: React.FC<Props> = ({ data }) => {
-  return (
-    <div>
-      <h1>Datos de Google Sheets</h1>
-      <table>
-        <thead>
-          <tr>
-            {data[4].map((header, index) => (
-              <th key={index}>{header}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.slice(5).map((row, rowIndex) => (
-            <tr key={rowIndex}>
-              {row.map((cell, cellIndex) => (
-                <td key={cellIndex}>{cell}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+export const revalidate = 60; // Opcional: Configura revalidación ISR (en segundos)
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const spreadsheetId = '15P5ZQ2BGTqbl8qmkz2Vt1VOaKPFyx1Df2W_KPf0kT_s';
-  const range = 'adults!A:D'; // Cambia el rango según tu hoja de cálculo
-
+export default async function InventoryPage() {
   const data = await getSheetData(spreadsheetId, range);
+  if (data){
+    return (
+        <div>
+          <h1>Datos de Google Sheets</h1>
+          <table>
+            <thead>
+              <tr>
+                {data[4].map((header: string, index: number) => (
+                  <th key={index}>{header}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {data.slice(5).map((row: string[], rowIndex: number) => (
+                <tr key={rowIndex}>
+                  {row.map((cell: string, cellIndex: number) => (
+                    <td key={cellIndex}>{cell}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
 
-  return {
-    props: {
-      data,
-    },
-  };
-};
+  }
 
-export default Home;
+
+}
--- lib/googleapi.ts
+++ lib/googleapi.ts
@@ -1,29 +1,21 @@
-const { google } = require('googleapis');
-const path = require('path');
-const fs = require('fs');
+// lib/googleSheets.ts
+import { google } from 'googleapis';
+import fs from 'fs';
+import path from 'path';
 
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+const keyFilePath = path.join(process.cwd(), 'credentials.json');
 
-const getClient = () => {
-  const content = fs.readFileSync(CREDENTIALS_PATH);
-  const credentials = JSON.parse(content);
+export async function getSheetData(spreadsheetId: string, range: string) {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: keyFilePath,
+    scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+  });
 
-  const { client_email, private_key } = credentials;
-  const auth = new google.auth.JWT(client_email, null, private_key, SCOPES);
-
-  return google.sheets({ version: 'v4', auth });
-};
-
-const getSheetData = async (spreadsheetId: any, range: any) => {
-  const sheets = getClient();
+  const sheets = google.sheets({ version: 'v4', auth });
   const response = await sheets.spreadsheets.values.get({
     spreadsheetId,
     range,
   });
-  return response.data.values;
-};
 
-module.exports = {
-  getSheetData,
-};
+  return response.data.values;
+}
