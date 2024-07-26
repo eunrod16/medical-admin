0d4c4bba11d3a139f6c7b599a0fd519c62bb98e9
eunicerodas <114092041+eunicerodas@users.noreply.github.com>
2024-07-26 16:05:07 -0600

    Update page.tsx

--- app/admin/inventory/page.tsx
+++ app/admin/inventory/page.tsx
@@ -10,9 +10,9 @@ export default async function InventoryPage() {
   return (
     <main className="flex flex-1 flex-col p-4 md:p-6">
       <div className="flex items-center mb-8">
-        <h1 className="font-semibold text-lg md:text-2xl">Lista Pacientes</h1>
+        <h1 className="font-semibold text-lg md:text-2xl">Lista Medicamentos</h1>
       </div>
-      <InventoryTable  />
+
 
     </main>
   );
