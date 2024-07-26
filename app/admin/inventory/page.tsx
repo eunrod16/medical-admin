5ce5534ee468e45cbf6f117aa6bf69db5f7ccce8
eunicerodas <114092041+eunicerodas@users.noreply.github.com>
2024-07-26 15:48:02 -0600

    Update page.tsx

--- app/admin/inventory/page.tsx
+++ app/admin/inventory/page.tsx
@@ -1,11 +1,19 @@
 // app/inventory/page.tsx
 import { getSheetData } from '@/lib/googleapi';
 import React from 'react';
+import {
+  Table,
+  TableHead,
+  TableRow,
+  TableHeader,
+  TableCell,
+  TableBody
+} from '@/components/ui/table'; // Asegúrate de ajustar esta ruta según tu estructura de proyecto
 
 const spreadsheetId = process.env.SPREADSHEET_ID || '15P5ZQ2BGTqbl8qmkz2Vt1VOaKPFyx1Df2W_KPf0kT_s'; // Utiliza una variable de entorno para mayor flexibilidad
 const range = 'adults!A:D'; // Cambia el rango según tu hoja de cálculo
 
-export const revalidate = 60; // Opcional: Configura revalidación ISR (en segundos)
+export const revalidate = 60; // Configura revalidación ISR (en segundos)
 
 export default async function InventoryPage() {
   const data = await getSheetData(spreadsheetId, range);
@@ -13,24 +21,24 @@ export default async function InventoryPage() {
     return (
       <div>
         <h1>Datos de Google Sheets</h1>
-        <table style={{ borderCollapse: 'collapse', width: '100%' }}>
-          <thead>
-            <tr>
+        <Table>
+          <TableHead>
+            <TableRow>
               {data[4].map((header: string, index: number) => (
-                <th key={index} style={{ border: '1px solid black', padding: '8px' }}>{header}</th>
+                <TableHeader key={index} className="border border-black p-2">{header}</TableHeader>
               ))}
-            </tr>
-          </thead>
-          <tbody>
+            </TableRow>
+          </TableHead>
+          <TableBody>
             {data.slice(5).map((row: string[], rowIndex: number) => (
-              <tr key={rowIndex}>
+              <TableRow key={rowIndex}>
                 {row.map((cell: string, cellIndex: number) => (
-                  <td key={cellIndex} style={{ border: '1px solid black', padding: '8px' }}>{cell}</td>
+                  <TableCell key={cellIndex} className="border border-black p-2">{cell}</TableCell>
                 ))}
-              </tr>
+              </TableRow>
             ))}
-          </tbody>
-        </table>
+          </TableBody>
+        </Table>
       </div>
     );
   }
